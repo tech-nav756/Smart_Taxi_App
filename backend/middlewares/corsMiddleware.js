783786ee37bcca6111aba0c56a1ea1c58b7f4a4e
moruditech <moruditech@gmail.com>
2025-03-20 11:39:15 +0000

    initialize frontend

--- backend/middlewares/corsMiddleware.js
+++ backend/middlewares/corsMiddleware.js
@@ -1,8 +1,13 @@
 const cors = require('cors');
+const dotenv = require('dotenv');
 
+// Load environment variables from .env file
+dotenv.config();
+
+// CORS middleware configuration
 const corsMiddleware = cors({
-  origin: ['https://trusted-frontend-domain.com'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  origin: ["https://animated-space-disco-x5p47pg7gqpwhv9v6-8081.github.dev"], // Default to localhost if BASE_URL is not set
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
